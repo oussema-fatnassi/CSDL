25826e31ffa272308559977d79b6b39b516925c1
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-05-06 15:23:02 +0200

    [changes] Changed the configuration file with windows

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,8 +1,22 @@
 #include <raylib.h>
 #include "menu.hpp"
+#include <iostream>
+#include "grid.hpp"
 
 void createButton(int x, int y, int width, int height, const char *text)
 {
     DrawRectangle(x, y, width, height, WHITE);
     DrawText(text, x + 10, y + 10, 20, BLACK);
+}
+
+void buttonIsClicked()
+{
+    if (CheckCollisionPointRec(GetMousePosition(), {50, 620, 100, 50}))
+    {
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            std::cout << "Button clicked" << std::endl;
+            randomSpawn(800, 600, 20);
+        }
+    }
 }
