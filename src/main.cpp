924a9d8c07c7e3d670f3f76cc7e08b02237fd3f7
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-05-07 18:03:59 +0200

    [changes] Added the default mode

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,6 @@ Menu currentMenu = MAIN_MENU;
 int main() {
     const int screenWidth = 800;
     const int screenHeight = 1000;
-    // const int gridWidth = 800;
-    // const int gridHeight = 800;
-    // const int cellSize = 20;
 
     InitWindow(screenWidth, screenHeight, "Game of Life");
 
@@ -40,27 +37,15 @@ int main() {
         case RANDOM_MODE:
             drawRandomMode();
             break;
+        case DEFAULT_MODE:
+            drawDefaultMode();
+            break;
+        default:
+            break;
     }
         menuInput();
         modeInput();
 
-        // createGrid(gridWidth, gridHeight, cellSize);
-        // createButton(50, 820, 100, 50, "Random");
-        // createButton(200, 820, 100, 50, "Default");
-        // createButton(350, 820, 100, 50, "Custom");
-        // createButton(50, 880, 180, 50, "Start Simulation");
-        // createButton(250, 880, 100, 50, "Stop");
-        // createButton(370, 880, 130, 50, "Clear Grid");
-        // spawnRandomButton();
-        // startSimulationButton();
-        // stopSimulationButton();
-        // clearGridButton();
-        // customButton();
-        // customGridSpawn();
-
-        // if (isSimulationRunning) {
-        //     applyConwayRules();
-        // }
         EndDrawing();
     }
 
