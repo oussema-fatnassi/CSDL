791922ed48d2305be3055b056a3d4da0073ceb1e
oussema-fatnassi <oussema.fatnassi@laplateforme.io>
2024-05-12 18:40:45 +0200

    [changes] Added images for button and hover

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -8,9 +8,31 @@ using namespace std;
 bool isCustomMode = false;
 
 
-void createButton(float x, float y, int width, int height, const char *text, Font font)
+void createButton(float x, float y, int width, int height, const char *text, Font font, int imageWidth, int imageHeight)
 {
-    DrawRectangle(x, y, width, height, WHITE);
+    Image buttonNormal = LoadImage("assets/images/button.png");
+    Texture2D buttonNormalTexture = LoadTextureFromImage(buttonNormal);
+
+    Image buttonHover = LoadImage("assets/images/buttonHover.png");
+    Texture2D buttonHoverTexture = LoadTextureFromImage(buttonHover);
+
+    // Define the source rectangle to use the entire button texture
+    Rectangle sourceRec = { 0.0f, 0.0f, (float)buttonNormal.width, (float)buttonNormal.height };
+    
+    // Define the destination rectangle based on the specified width and height
+    Rectangle destRec = { x, y, (float)width, (float)height };
+    
+    // Check if the mouse is hovering over the button
+    if (CheckCollisionPointRec(GetMousePosition(), destRec))
+    {
+        // Draw the hovered button image texture
+        DrawTexturePro(buttonHoverTexture, sourceRec, destRec, {0, 0}, 0, WHITE);
+    }
+    else
+    {
+        // Draw the normal button image texture
+        DrawTexturePro(buttonNormalTexture, sourceRec, destRec, {0, 0}, 0, WHITE);
+    }
     
     // Calculate the width of the text
     int textWidth = MeasureTextEx(font, text, 20, 0).x;
@@ -19,11 +41,13 @@ void createButton(float x, float y, int width, int height, const char *text, Fon
     float xPos = x + (width - textWidth) / 2;
     
     // Calculate the y-coordinate to center the text vertically
-    float yPos = y + (height - 20) / 2; // Assuming font size is 20
+    float yPos = y + (height - 20) / 2; 
 
     // Draw the text using the specified font
     DrawTextEx(font, text, { xPos, yPos }, 20, 0, BLACK);
 }
+
+
 
 
 void clearGridButton()
--- src/buttons.hpp
+++ src/buttons.hpp
@@ -1,7 +1,7 @@
 #ifndef BUTTONS_H
 #define BUTTONS_H
 
-void createButton(float x, float y, int width, int height, const char *text, Font font);
+void createButton(float x, float y, int width, int height, const char *text, Font font, int imageWidth, int imageHeight);
 void clearGridButton();
 
 #endif  // BUTTONS_H
